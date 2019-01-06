35ff6636435155e063d699e0d40aaac1b85196e9
unknown <002myusuf@gmail.com>
2019-01-06 12:40:29 +0700

    Replace

--- aboutus/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def labView(request):
-    return render(request, 'aboutus/aboutus.html')
--- publications/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def pubView(request):
-    return render(request, 'publications/publications.html') 
